833ee65800afe092f468d1226a340e2fc17c5f49
Vivek Mara <135492014+VivekMara@users.noreply.github.com>
2024-02-04 11:32:45 +0530

    Delete FlappyBird.py

--- FlappyBird.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-
-root = tk.Tk()
-
-root.title("Hello, Welcome to the World of Flappy bird")
-root.geometry("800x600")
-label = tk.Label(root, text="Welcome to Tkinter!")
-label.pack()
-root.mainloop()
